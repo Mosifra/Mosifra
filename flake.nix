bd1b7bb644b921d0d782df274534101e0bb9f55b
Agahnim <gabrielguysoulard@gmail.com>
2026-01-12 15:19:05 +0100

    a

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [docker_28];
+      shellHook = ''
+        echo "Veuillez exécuter la commande sudo dockerd puis lancer un autre shell pour développer"
+      '';
+    };
+  };
+}
